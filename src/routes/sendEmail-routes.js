0ef506ae6f6a3623bdbcc64ebe9205a6373f81d0
Juan Camacaro <juanc.camacaro@gmail.com>
2025-03-02 16:24:27 -0500

    export routes send email module

--- src/routes/sendEmail-routes.js
+++ src/routes/sendEmail-routes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from "express";
 import { methods as sendRecoveryCode } from '../controllers/sendEmail-controllers.js';
 const router = express.Router();
 
@@ -6,4 +6,4 @@ router.post("/send-recovery-code", sendRecoveryCode.sendRecoveryEmailCode);
 router.post("/check-email", sendRecoveryCode.checkEmailExists);
 router.patch("/resetkey/:email", sendRecoveryCode.resetKey);
 
-module.exports = router;
+export default router;
