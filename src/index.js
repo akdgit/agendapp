73509d6b6f7ea982dceca03088522a2b72e4c2fe
Juan Camacaro <juanc.camacaro@gmail.com>
2025-04-10 09:31:45 -0500

    Backend modifies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import app from "./app.js";
 
-const main = () => {
+//Para configración en railway
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+//Local
+/*const main = () => {
     app.listen(app.get("port"));
     console.log(`Server on port:  ${app.get("port")}`);
-}
+}*/
 
 main()
